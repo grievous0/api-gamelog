33b47437bbffd312217e016558bd9c1c21f381d4
grievous0 <jvopereira2004@gmail.com>
2023-03-13 19:46:32 -0300

    Adjust Dockerfile

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,12 @@ dotenv.config()
 import express from 'express'
 import { router } from './routes'
 
+const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(express.json());
 app.use(router);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
