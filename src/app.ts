d29c56b636abe14be179b2be93744608be599014
grievous0 <jvopereira2004@gmail.com>
2023-03-14 18:36:16 -0300

    Fix cors

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import express from 'express'
+import cors from 'cors'
 import { router } from './routes'
 
 const PORT = process.env.PORT || 3000
 const app = express()
 
+app.use(cors())
 app.use(express.json());
 app.use(router);
 
